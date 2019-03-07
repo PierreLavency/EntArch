30243bf4caeebc2b5e9d006d9e7dab1c6e9a0a68
Pierre Lavency <pierrelavency@hotmail.com>
2019-03-07 16:14:10 +0100

    to test on collab

--- setup_google_colab.py
+++ setup_google_colab.py
@@ -3,27 +3,25 @@ import os
 def download_github_code(path):
     filename = path.rsplit("/")[-1]
     os.system("shred -u {}".format(filename))
-    course = 'bayesian-methods-for-ml'
+    rep = 'EntArch'
     branch = 'master'
-    url = 'https://raw.githubusercontent.com/hse-aml/{}/{}/{}'
+    url = 'https://raw.githubusercontent.com/PierreLavency/{}/{}/{}'
     template = "wget '" + url + "' -O '{}'"
-    os.system(template.format(course, branch, path, filename))
-
+    os.system(template.format(rep, branch, path, filename))
 
 def download_github_lfs(path):
     filename = path.rsplit("/")[-1]
     os.system("shred -u {}".format(filename))
-    course = 'bayesian-methods-for-ml'
+    rep = 'EntArch'
     branch = 'master'
-    url = "https://github.com/hse-aml/{}/blob/{}/{}?raw=true"
+    url = "https://github.com/PierreLavency/{}/blob/{}/{}?raw=true"
     template = "wget '" + url + "' -O '{}'"
-    os.system(template.format(course, branch, path, filename))
+    os.system(template.format(rep, branch, path, filename))
 
-
-def load_data_week2():
-    download_github_code("week2/w2_grader.py")
-    download_github_code("week2/samples.npz")
-
+def load_data(dim=26)
+    path="/Data/CBL_"+str(dim)+"_features.csv"
+    download_github_code(path)
+        
 def load_data_final_project():
     download_github_code("week7_(final_project)/utils.py")
     download_github_lfs("week7_(final_project)/CelebA_VAE_small_8.h5")
